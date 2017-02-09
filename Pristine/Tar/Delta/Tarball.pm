4ef16628b9fc51f8b138d5637811c8c740efc3b2
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2017-02-09 13:18:13 +0100

    Use temporary delta directory for tarballs containing pristine deltas.

--- Pristine/Tar/Delta/Tarball.pm
+++ Pristine/Tar/Delta/Tarball.pm
@@ -13,7 +13,7 @@ sub write {
 	my $deltafile=shift;
 	my $delta=shift;
 
-	my $tempdir=tempdir();
+	my $tempdir=tempdir_delta();
 
 	foreach my $field (keys %$delta) {
 		if (Pristine::Tar::Delta::is_filename($field)) {
@@ -35,7 +35,7 @@ sub read {
 	my $class=shift;
 	my $deltafile=shift;
 	
-	my $tempdir=tempdir();
+	my $tempdir=tempdir_delta();
 	doit("tar", "xf", File::Spec->rel2abs($deltafile), "-C", $tempdir);
 
 	my %delta;
